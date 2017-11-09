527d203b5efd4319a064fa9ffee0f25f338e53b0
cwendlan <cwendlan@unb.ca>
2017-11-09 15:38:39 -0400

    Changing to print full name

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Cassie");
+		System.out.println("Hello Cassandra Wendlandt-Bloodsworth");
 	}
 
 }
